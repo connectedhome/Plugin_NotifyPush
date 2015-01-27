f256094e5c4be657e9b3a28fb9e2feb02f5e7c9d
Yaswanth <yaswanthsvist@gmail.com>
2015-01-27 10:25:10 +0530

    Update pushGCM.rb

--- Example/server/pushGCM.rb
+++ Example/server/pushGCM.rb
@@ -2,8 +2,10 @@ require 'rubygems'
 require 'pushmeup'
 GCM.host = 'https://android.googleapis.com/gcm/send'
 GCM.format = :json
-GCM.key = "API_KEY_GOES_HERE"
-destination = ["REGISTRATION_ID_GOES_HERE"]
-data = {:message => "PhoneGap Build rocks!", :msgcnt => "1", :soundname => "beep.wav"}
-
+GCM.key = "AIzaS23nNmx21ojsMqoJhtenGGt4JAn9BhTa0qQ"
+destination = ["APA91bEBWuhdT8aFr1exUqicephXk2N0TJZgIK1RVy3Gf5Md_9a_1JGWbf3ka3bUOJ0HkQcziMvGg9gbIIe6ELduin1G0qzutENdrQEUVei8nCiLLZsyvOmaWsHVJ3n9PiRXVlgLkQHON6bQ_MOsb422WzjtFIhGHp1SQ95mcDZkwV3CBMBv6Xc"]
+data = {
+  :message=>"DEVICE_BATTERY_FAILED"
+}
 GCM.send_notification( destination, data)
+
